04a51d4bcdbfd75cbffdbd49a4a76d422b5a9d65
Tomek Zając <tomezpl@gmail.com>
2015-10-22 21:51:42 +0100

    Added a login script

---
+++ login.php
@@ -0,0 +1,13 @@
+﻿<?php
+	if(strpos(file_get_contents("userlist"), $_GET["userName"] . "\r\n") == false)
+	{
+		$userlist = fopen("userlist", "a+");
+		$msgBuffer = fopen("msgBuffer", "a+");
+		
+		fwrite($userlist, $_GET["userName"] . "\r\n");
+		fwrite($msgBuffer, "\r\n" . $_GET["userName"] . " has logged in\r\n");
+		
+		fclose($userlist);
+		fclose($msgBuffer);
+	}
+?>
